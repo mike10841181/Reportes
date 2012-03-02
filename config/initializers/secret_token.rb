a884074ef7c450c04127e60a32b442b776099b1c
Mike Andino <mikeandinog@Ubuntu-G60VX.(none)>
2012-03-02 02:17:51 -0600

    primer push de los reportes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reporte::Application.config.secret_token = '9f6ee2c9414de98cbfc8bddb8cf0d388d8b3888ffd688033c4ed4ed56ab0ea51678217d5d10d84a043413cc584c7cc6a7aecbe099fd9e4e7aee02090aef71394'
